c2facd3ba64a73ba72fe88172a7cc73c453ec73d
alinag <alina.levitin123@gmail.com>
2024-02-26 13:00:21 +0200

    lg_gui.py - works scale_helical_track_length.py - works Segment_average_generation.py - in process

--- 1_preprocessing/1_c_Segment_average_generation/Segment_average_generation.py
+++ 1_preprocessing/1_c_Segment_average_generation/Segment_average_generation.py
@@ -1,17 +1,19 @@
 import LG_MiRP
 
 
-segment_average_generation_gui = LG_MiRP.LgGui(title_name="Segment Average")
-
-segment_average_generation_gui.add_image(image_name="segment_average.jpg")
+segment_average_generation_gui = LG_MiRP.LgGui(title_name="Segment Average", image_name="segment_average.jpg")
 
 segment_average_generation_gui.add_sub_job_name('Scale helical track length', row=2)
 
-input_star_file = segment_average_generation_gui.add_star_file_entry('Select a particles.star file', row=3)
+input_star_file = segment_average_generation_gui.add_file_entry('star', 'Select a particles.star file', row=3)
 
 scale_entry = segment_average_generation_gui.add_number_entry("Scale factor (example: 0.25)", row=4)
 
 segment_average_generation_gui.add_run_button(lambda: LG_MiRP.scale(input_star_file, float(scale_entry.get())), row=5)
+
+segment_average_generation_gui.add_sub_job_name('Segment average generation', row=6)
+
+directory = segment_average_generation_gui.add_directory_entry('Select directory containing extracted particles (Extract)',row=7)
 
 
 segment_average_generation_gui.mainloop()
--- LG_MiRP/gui_base/lg_gui.py
+++ LG_MiRP/gui_base/lg_gui.py
@@ -1,41 +1,41 @@
 """
 Author: Alina Levitin
-Date: 22/02/24
+Date: 26/02/24
 Version: 1.0
 """
 
 import tkinter as tk
 from tkinter import filedialog
 import pkg_resources
-from PIL import ImageTk, Image
-
-import starfile
+from PIL import ImageTk, Image #pillow
 
 
 class LgGui(tk.Tk):
-    def __init__(self, title_name: str = "empty", **kwargs):
+    def __init__(self, title_name: str = "empty", image_name: str = "missing image", **kwargs):
         super().__init__()
 
-        self.geometry("630x800")
+        # self.geometry("630x800")
         self.title(title_name)
         self.add_job_name(title_name)
+        self.add_exit_button()
+
+        self.image = self.resize_image(image_name, 600)
+        self.browse_image = self.resize_image('directory_icon.png', 20)
+
+        self.add_image()
 
     def add_job_name(self, title):
         label = tk.Label(self, text=title, font=('Ariel', 18))
         label.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
 
-    def add_image(self, image_name: str = "missing image"):
-        image_stream = pkg_resources.resource_stream('LG_MiRP', f'assets/{image_name}')
-        image = Image.open(image_stream)
+    def add_image(self, row: int = 10):
 
-        scale = float(600 / image.width)
-        new_width = 600  # Set the desired width
-        new_height = int(image.height*scale)  # Set the desired height
-        resized_image = image.resize((new_width, new_height))
+        image_label = tk.Label(self, image=self.image)
+        image_label.grid(row=row, column=0, columnspan=5, padx=5, pady=5)
 
-        self.img = ImageTk.PhotoImage(resized_image)
-        self.imlabel = tk.Label(self, image=self.img)
-        self.imlabel.grid(row=7, column=0, columnspan=4, padx=5, pady=5)
+    def add_exit_button(self, row: int = 11):
+        button = tk.Button(self, text='Exit', command=self.destroy)
+        button.grid(row=row, column=5, columnspan=1, padx=5, pady=5)
 
     def add_sub_job_name(self, name: str = 'test', row: int = 2):
         label = tk.Label(self, text=name, font=('Ariel', 16))
@@ -43,27 +43,48 @@ class LgGui(tk.Tk):
 
     def add_run_button(self, command, row):
         button = tk.Button(self, text='Run', command=command)
-        button.grid(row=row, column=0, columnspan=4, padx=5, pady=5)
+        button.grid(row=row, column=1, columnspan=1, padx=5, pady=5)
 
     @staticmethod
-    def browse_star_files(file_type, file_entry):
+    def browse(file_type, file_entry=None):
+        if file_type == 'directory':
+            directory = filedialog.askdirectory(title="Select directory")
+            if directory:
+                file_entry.delete(0, tk.END)
+                file_entry.insert(0, directory)
+            return directory
         # Open file dialog to select STAR file
         file_path = filedialog.askopenfilename(filetypes=[(f"{file_type.upper()} Files", f"*.{file_type}")])
         if file_path:
             file_entry.delete(0, tk.END)
             file_entry.insert(0, file_path)
 
-    def add_star_file_entry(self, entry_name, row):
+    def add_file_entry(self, entry_type, entry_name, row):
         label = tk.Label(self, text=entry_name, font=('Ariel', 12))
         label.grid(row=row, column=0, padx=5, pady=5)
 
         star_entry = tk.Entry(self, width=50)
         star_entry.grid(row=row, column=1, padx=5, pady=5)
 
-        browse_button = tk.Button(self, text="Browse", command=lambda: self.browse_star_files("star", star_entry))
+        browse_button = tk.Button(self, text="Browse", command=lambda: self.browse(entry_type, star_entry))
         browse_button.grid(row=row, column=2, padx=5, pady=5)
 
         return star_entry
+
+    def add_directory_entry(self,entry_name, row):
+
+        label = tk.Label(self, text=entry_name, font=('Ariel', 12))
+        label.grid(row=row, column=0, padx=5, pady=5)
+
+        browse_label = tk.Label(self, image=self.browse_image)
+        browse_label.grid(row=row, column=3, columnspan=4, padx=5, pady=5)
+
+        directory = tk.Entry(self, width=50)
+        directory.grid(row=row, column=1, padx=5, pady=5)
+        browse_button = tk.Button(self, text="Browse", command=lambda: self.browse('directory', directory))
+        browse_button.grid(row=row, column=2, padx=5, pady=5)
+
+        return directory
 
     def add_number_entry(self, entry_name, row):
         label = tk.Label(self, text=entry_name, font=('Ariel', 12))
@@ -73,3 +94,20 @@ class LgGui(tk.Tk):
         number_entry.grid(row=row, column=1, padx=5, pady=5)
 
         return number_entry
+
+    @staticmethod
+    def resize_image(image_name: str = "missing image", new_width: int = 100):
+        image_stream = pkg_resources.resource_stream('LG_MiRP', f'assets/{image_name}')
+        if image_stream:
+            image = Image.open(image_stream)
+
+            scale = new_width / image.width
+            new_height = int(image.height * scale)
+            resized_image = image.resize((new_width, new_height))
+
+            new_image = ImageTk.PhotoImage(resized_image)
+
+            return new_image
+
+
+
