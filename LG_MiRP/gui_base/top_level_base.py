a5dbdbfeb6bd0473e768767396b6e1ca54af7528
alinag <alina.levitin123@gmail.com>
2024-03-11 16:14:23 +0200

    added tk.TopLevel class top_level_base.py for generating secondary windows

--- LG_MiRP/gui_base/top_level_base.py
+++ LG_MiRP/gui_base/top_level_base.py
@@ -1,3 +1,10 @@
+"""
+Author: Alina Levitin
+Date: 11/03/24
+Updated: 11/3/24
+
+Class to generate tk.TopLevel LG-style.
+"""
 import tkinter as tk
 
 from LG_MiRP.gui_base import utils
@@ -5,19 +12,33 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class LGTopLevelBase(tk.Toplevel):
+    """
+    Class to generate tk.TopLevel LG style
+    To create secondary windows
+    """
     def __init__(self, master):
+        """
+        :param master: the master from which the secondary is generated from
+        """
         super().__init__(master)
         self.image = None
+
+        # Creating an exit button
         self.add_exit_button()
 
     def add_plot(self, fig, row: int = 0):
+        """
+        Adding a plot to the secondary window
+        :param fig: plt fig
+        :param row: the row in which to display the plot
+        """
         canvas = FigureCanvasTkAgg(fig, master=self)
         canvas.draw()
         canvas.get_tk_widget().grid(row=row, column=0)
 
     def add_image(self, image_name: str = "default_image.jpg", new_size: int = 600, row: int = 1):
         """
-        Adds the resized image to the GUI window, row is set to 10
+        Adds the resized image to the GUI window, row is set to 1
 
         :param image_name: selected image name
         :param new_size: in case image needs to be resized, default is 600 since it looks nice
