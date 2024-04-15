45fe0b6b939ea4378979c34e9d6911dd580b78b6
alinag <alina.levitin123@gmail.com>
2024-04-15 13:28:53 +0300

    Added keep PHI shifts and angles reset method in global search

--- LG_MiRP/gui/__init__.py
+++ LG_MiRP/gui/__init__.py
@@ -2,3 +2,4 @@ from .segment_average_gui import *
 from .class_uni_ext_gui import *
 from .rescale_references_gui import *
 from .reset_shifts_angles_gui import *
+from .keep_PHI_reset_shifts_angles_gui import *
---
+++ LG_MiRP/gui/keep_PHI_reset_shifts_angles_gui.py
@@ -0,0 +1,51 @@
+"""
+Author: Alina Levitin
+Date: 15/04/24
+Updated: 15/04/24
+
+Two GUI classes (master and frame) for shifts and angle reset
+The method of shifts and angle reset is in and extraction is located in LG_MiRP/methods/reset_angles_shifts
+
+shifts: _rlnOriginX, _rlnOriginY, _rlnOriginZ are set to 0
+(_rlnAngleRot is kept same)
+angles: _rlnAnglePsi, _rlnAngleTilt are set to priors: _rlnAnglePsiPrior, _rlnAngleTiltPrior
+"""
+from ..gui_base import LgFrameBase, LgMasterGui
+from ..methods import keep_PHI_reset_angles_and_translations
+
+
+class KeepPHIResetShiftsAnglesGui(LgMasterGui):
+    """
+    ...
+    Inherits from LgMasterGui
+    """
+    def __init__(self):
+        super().__init__()
+        self.add_job_name("Keep PHI, reset shifts to 0 and angles to prior")
+        frame = ResetShiftsAnglesFrame(self)
+        frame.grid(row=1, column=0, sticky="NSEW")
+        self.mainloop()
+
+
+class ResetShiftsAnglesFrame(LgFrameBase):
+    """
+    ...
+    Inherits from LgFrameBase
+    """
+    def __init__(self, master):
+        """
+        :param master: the master gui in which the frame will be displayed
+        """
+        super().__init__(master)
+        # Creates an entry for run_it000_data.star file
+        input_star_file = self.add_file_entry('star', 'Select a run_it001_data.star file', row=1)
+
+        # Creates an entry for output directory
+        output_directory = self.add_directory_entry('Select output directory', row=2)
+
+        # Creates a "Run" button that uses the class unification and extraction method
+        self.add_run_button(lambda: keep_PHI_reset_angles_and_translations(input_star_file, output_directory),
+                            row=3)
+
+        # Imports a themed image at the bottom
+        self.add_image(new_size=600, row=4)
