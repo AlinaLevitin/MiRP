898385412e9ed0ef8778afe6ee3fc73dc0476cfd
Malinka <alina.levitin123@gmail.com>
2024-05-01 14:36:15 +0300

    cleaned up init

--- LG_MiRP/methods/__init__.py
+++ LG_MiRP/methods/__init__.py
@@ -3,8 +3,6 @@ from .scale_helical_track_length import *
 from .class_unifier_extractor import *
 from .reference_scaler import *
 from .reset_angles_shifts import *
-from .angle_smoothing import *
-from .shifts_smoothing import *
 from .angles_shifts_correction import *
 from .angles_and_shifts_smoothing import *
 from .methods_utils import *
